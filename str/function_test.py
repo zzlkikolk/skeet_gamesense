74f163f5bff2992215b58bd2252461913e065b00
zhangzhilin <zhangzhilin@igamebuy.com>
2024-09-18 10:57:37 +0800

    学习Python 函数定义-不定参数长度

--- str/function_test.py
+++ str/function_test.py
@@ -8,6 +8,14 @@ def default_value_function(str1='zhang'):
     print(str1)
 
 
+# 不定参数长度
+def args_function(args1, *vartuples):
+    print("\n不定参数长度")
+    print(args1)
+    print(*vartuples)
+
+
 if __name__ == '__main__':
     my_function("zhang1")
     default_value_function()
+    args_function(1,12,23)
--- str/myList.py
+++ str/myList.py
@@ -1,9 +1,9 @@
-
 def print_format(num):
     print('打印数字{}'.format(num))
 
+
 if __name__ == '__main__':
-    list = [1,2,3,4]
+    list = [1, 2, 3, 4]
     print(list[1:3])
     for item in list:
         print_format(item)
