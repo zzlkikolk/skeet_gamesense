2f5776b3fe7f5f88b3350a624a9486a3338c864c
zhangzhilin <zhangzhilin@igamebuy.com>
2024-09-18 10:22:39 +0800

    学习Python for循环 函数定义

--- str/myList.py
+++ str/myList.py
@@ -1,5 +1,11 @@
+
+def print_format(num):
+    print('打印数字{}'.format(num))
+
 if __name__ == '__main__':
     list = [1,2,3,4]
     print(list[1:3])
     for item in list:
-        print('for循环{}'.format(item))
+        print_format(item)
+    else:
+        print("for循环结束")
