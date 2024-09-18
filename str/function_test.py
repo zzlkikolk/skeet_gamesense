ee2ac285fd4819208e08603cdfb421a55392188e
zhangzhilin <zhangzhilin@igamebuy.com>
2024-09-18 10:42:17 +0800

    学习Python 函数定义

---
+++ str/function_test.py
@@ -0,0 +1,13 @@
+# 需要传入参数
+def my_function(str):
+    print(str)
+
+
+# 给定参数默认值
+def default_value_function(str1='zhang'):
+    print(str1)
+
+
+if __name__ == '__main__':
+    my_function("zhang1")
+    default_value_function()
