e9132af8b4447f2571a4b467c2bdffb1d3dfdfbb
zhangzhilin <zhangzhilin@igamebuy.com>
2024-09-19 11:57:07 +0800

    学习Python 爬虫-设置请求头

--- pacong/http_pa_test.py
+++ pacong/http_pa_test.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 
 def fetch_webpage(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/58.0.3029.110 Safari/537.36'}
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # 检查请求是否成功
         return response.text
     except requests.RequestException as e:
